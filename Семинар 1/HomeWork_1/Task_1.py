d5c528101ab5706dd7254155132c63c8890134b6
MichaelPRV <michelin7@rambler.ru>
2022-12-16 11:31:24 +0300

    initial commit

--- Семинар 1/HomeWork_1/Task_1.py
+++ Семинар 1/HomeWork_1/Task_1.py
@@ -1,13 +1,1 @@
 
-# Задача 1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-weekday = int(input("Input day of a week number: "))
-
-if 0 < weekday < 6:
-    print("The day is not weekend")
-elif 5 < weekday < 8:
-    print("The day is weekend")
-else:
-    print("Incorrect input!")
-
-# проверка связи во воторой раз
